857c7b18f413a9b21ef93340529b3f0272d111c6
Rorabaugh <rorabaugh@aspire-5734z-lnx.msu.montana.edu>
2020-03-12 07:44:23 -0600

    my_commit

--- src/Client.cpp
+++ src/Client.cpp
@@ -17,30 +17,351 @@
 #include "common.hpp"
 #include "Client.hpp"
 
-Client::~Client() {
+using namespace std ;
+
+string ascii ;
+
+Client::~Client()
+{
+	// remove action board
+	
+	remove ( board_name . c_str ( ) ) ;
+	
 }
 
 
-void Client::initialize(unsigned int player, unsigned int board_size){
+void Client::initialize(unsigned int player, unsigned int board_size)
+{
+	// set data: 
+	this -> player 		= player ;
+	this -> board_size	= board_size ;
+	
+	// create action board (player_#.action_board.json:
+	
+	// create row vector:
+	
+	vector < int > row ( board_size , 0 ) ;
+	
+	// create vector of vectors:
+	
+	vector < vector < int > > action_board ( board_size , row ) ;
+	
+	// write to file
+	
+	if ( player == 1 )
+	{		
+		board_name = "player_1.action_board.json" ;
+	}
+	else if ( player == 2 )
+	{
+		board_name = "player_2.action_board.json" ;
+	}
+	
+	// remove any old serialization files:
+	
+	remove ( board_name . c_str ( ) ) ;									
+	
+	// create an output file stream :
+	
+	ofstream file_out ( board_name ) ; 	
+
+	// must include braces so destructor of archive is called before we close the file:
+	{
+		// initialize an archive on the file:
+		
+		cereal :: JSONOutputArchive archive_out ( file_out ) ; 	
+		
+		// serialize the data giving it a name:
+		
+		archive_out ( cereal :: make_nvp ( "board" , action_board ) ) ; 			
+	}
+	// destructor will have been called
+	
+	// close the file:
+	
+	file_out . close ( ) ; 		
+
+	// resize string for ascii :
+	
+	ascii . resize ( ( board_size + 1 ) * board_size ) ;
+
+	this -> initialized = true ;
 }
 
 
-void Client::fire(unsigned int x, unsigned int y) {
+void Client::fire(unsigned int x, unsigned int y)
+{
+	// fires shot
+	
+	// creates player_#.shot.json file:
+	
+	string file_out_name ;		
+	if ( player == 1 )
+	{		
+		file_out_name = "player_1.shot.json" ;
+	}
+	else if ( player == 2 )
+	{
+		file_out_name = "player_2.shot.json" ;
+	}
+	
+	// remove any old serialization files:
+	
+	remove ( file_out_name . c_str ( ) ) ;										
+	
+	// create an output file stream:
+	
+	ofstream file_out ( file_out_name ) ; 
+
+	// must include braces so destructor of archive is called before we close the file:
+	{
+		// initialize an archive on the file:
+		
+		cereal :: JSONOutputArchive archive_out ( file_out ) ; 			
+		
+		// serialize the data giving it a name:
+		
+		archive_out ( cereal :: make_nvp ( "x" , x ) , cereal :: make_nvp ( "y" , y ) ) ; 				
+	}
+	// destructor will have been called
+	
+	// close the file:
+	
+	file_out . close ( ) ; 	
+	
 }
 
 
-bool Client::result_available() {
+bool Client::result_available()
+{
+	// check is result is available:
+	
+	string file_in_name ;
+	if ( player == 1 )
+	{		
+		file_in_name = "player_1.result.json" ;
+	}
+	else if ( player == 2 )
+	{
+		file_in_name = "player_2.result.json" ;
+	}
+	
+	// create an input file stream:
+	
+	ifstream file_in ( file_in_name ) ;
+	
+	// if file exists:
+	
+	if ( file_in )						
+	{
+		//close file:
+		
+		file_in . close ( ) ;	
+		
+		// return true :
+		
+		return true ;
+	}
+	
+	// otherwise return false:
+	
+	return false ;
 }
 
 
-int Client::get_result() {
+int Client::get_result()
+{
+	// get result from player_#.result.json file:
+	
+	// open json file:
+	
+	string file_in_name ;
+	if ( player == 1 )
+	{		
+		file_in_name = "player_1.result.json" ;
+	}
+	else if ( player == 2 )
+	{
+		file_in_name = "player_2.result.json" ;
+	}
+	
+	// create an input file stream:
+	
+	ifstream file_in ( file_in_name ) ;
+
+	if ( file_in )						
+	{
+		// initialize an archive on the file:
+		
+		cereal :: JSONInputArchive archive_in ( file_in ) ;		
+
+		// read result:
+		
+		int res;
+		
+		archive_in ( res ) ;
+		
+		//close file:
+		
+		file_in . close ( ) ;								
+		
+		// remove result file:
+		
+		remove ( file_in_name . c_str ( ) ) ;	
+		
+		if ( res != HIT && res != MISS && res != OUT_OF_BOUNDS )
+		{
+			throw ClientException ( "Bad result!\n" ) ;
+		}
+		
+		return res ;		
+	}
+	
+	throw ClientException ( "No result file!\n" ) ;
+	
+	return 0 ;
+	
+	// return HIT MISS or OUT_OF_BOUNDS
+	
+	return OUT_OF_BOUNDS ;
 }
 
 
 
-void Client::update_action_board(int result, unsigned int x, unsigned int y) {
+void Client::update_action_board(int result, unsigned int x, unsigned int y)
+{
+	// update internal representation of player_#.action_board.json
+	
+	// create row vector:
+	
+	vector < int > row ( board_size , 0 ) ;
+	
+	// create vector of vectors:
+	
+	vector < vector < int > > action_board ( board_size , row ) ;
+	
+	// read old action board:
+	
+	ifstream file_in ( board_name ) ;
+
+	if ( file_in )						
+	{
+		// initialize an archive on the file:
+		
+		cereal :: JSONInputArchive archive_in ( file_in ) ;		
+		
+		// deserialize data:
+		
+		archive_in ( action_board ) ;
+	}
+	
+	//close file:
+	
+	file_in . close ( ) ;
+	
+	// remove any old serialization files:
+	
+	remove ( board_name . c_str ( ) ) ;	
+
+	// modify vector:
+	
+	action_board [ x ] [ y ] = result ;
+	
+	// create an output file stream :
+	
+	ofstream file_out ( board_name ) ; 	
+
+	// must include braces so destructor of archive is called before we close the file:
+	{
+		// initialize an archive on the file:
+		
+		cereal :: JSONOutputArchive archive_out ( file_out ) ; 	
+		
+		// serialize the data giving it a name:
+		
+		archive_out ( cereal :: make_nvp ( "board" , action_board ) ) ; 			
+	}
+	// destructor will have been called
+	
+	// close the file:
+	
+	file_out . close ( ) ; 		
 }
 
 
-string Client::render_action_board(){
+string Client::render_action_board()
+{
+	// format ASCII representation of player_#.action_board.json
+	
+	// create row vector:
+	
+	vector < int > row ( board_size , 0 ) ;
+	
+	// create vector of vectors:
+	
+	vector < vector < int > > action_board ( board_size , row ) ;
+	
+	// read old action board:
+	
+	ifstream file_in ( board_name ) ;
+
+	if ( file_in )						
+	{
+		// initialize an archive on the file:
+		
+		cereal :: JSONInputArchive archive_in ( file_in ) ;		
+		
+		// deserialize data:
+		
+		archive_in ( action_board ) ;
+	}
+	
+	file_in . close ( ) ;
+	
+	unsigned int x = 0 ;
+	unsigned int y = 0 ;
+	unsigned int i = 0 ;
+	
+	while ( y < board_size - 1 )
+	{
+		while ( x < board_size )
+		{
+			int d = action_board [ x ] [ y ] ;
+			
+			// water:
+			if ( d == 0 )
+			{
+				ascii[i] = '-' ;
+			}
+			
+			// miss:
+			if ( d == -1 )
+			{
+				ascii[i] = 'X' ;
+			}
+			
+			// hit:
+			if ( d == 1 )
+			{
+				ascii[i] = '*' ;
+			}						
+			i ++ ;
+			x ++ ;
+		}		
+		
+		ascii[i] = '\n' ;
+		i ++ ;
+		
+		x = 0 ;
+		y ++ ;
+	}
+	
+	// return string version of board
+	
+	return ascii ;
+	
+	
 }
+
+
+
--- src/Server.cpp
+++ src/Server.cpp
@@ -18,6 +18,8 @@
 #include "common.hpp"
 #include "Server.hpp"
 
+using namespace std ;
+
 
 /**
  * Calculate the length of a file (helper function)
@@ -25,20 +27,256 @@
  * @param file - the file whose length we want to query
  * @return length of the file in bytes
  */
-int get_file_length(ifstream *file){
+int get_file_length ( ifstream *file )
+{
+    file -> seekg ( 0 , ios :: end ) ;
+	
+    int l = file -> tellg ( ) ;
+	
+    return l ;
 }
 
 
-void Server::initialize(unsigned int board_size,
-                        string p1_setup_board,
-                        string p2_setup_board){
+void Server :: initialize ( unsigned int board_size , string p1_setup_board, string p2_setup_board )
+{
+	
+	// open board files and verify sizes:
+		
+	this -> p1_setup_board . open ( p1_setup_board , ifstream::in ) ;
+	
+	if ( ! this -> p1_setup_board . is_open ( ) )
+	{
+		throw ServerException ( "Bad player 1 board file name: " + p1_setup_board + "!" ) ;
+	}
+	
+	this -> p2_setup_board . open ( p2_setup_board , ifstream::in ) ;
+	
+	if ( ! this -> p2_setup_board . is_open ( ) )
+	{
+		throw ServerException ( "Bad player 2 board file name: " + p2_setup_board + "!" ) ;
+	}
+	
+	// get file lengths:
+	
+	int l1 = get_file_length ( & this -> p1_setup_board ) ;
+	int l2 = get_file_length ( & this -> p2_setup_board ) ;
+	
+	// check that file lengths match board size:
+	
+	int	z = ( board_size + 1 ) * board_size ;
+	
+	if ( l1 != z )
+	{
+		throw ServerException ( "Wrong player 1 board size!\n" ) ;
+	}
+	
+	if ( l2 != z )
+	{
+		throw ServerException ( "Wrong player 2 board size!\n" ) ;
+	}
+	
+	// copy board_size:
+	
+	this -> board_size = board_size ;	
+}
+
+// get char value at given coordinates:
+char get_val ( ifstream *board , int board_size , unsigned int x , unsigned int y )
+{
+	// calculate the linear point 'p' that we wish to read:
+	
+	unsigned int p = ( board_size + 1 ) * y + x ;
+	
+	// seek to p:
+	
+	board -> seekg ( p ) ;
+
+	// get the correct char at p:
+	
+	char c ;	
+	board -> get ( c ) ;				
+	
+	// return char:
+	
+	return c ;
 }
 
 
-int Server::evaluate_shot(unsigned int player, unsigned int x, unsigned int y) {
+int Server :: evaluate_shot ( unsigned int player , unsigned int x , unsigned int y )
+{
+	// check if valid player number:
+	
+	if ( player <= 0 )
+	{
+		throw ServerException ( "Player number too low!\n" ) ;
+	}
+	
+	if ( player > MAX_PLAYERS )
+	{
+		throw ServerException ( "Player number too high!\n" ) ;
+	}	
+	
+	// check if shot is within bounds:
+	
+	if ( x < 0 )
+	{
+		return OUT_OF_BOUNDS ;
+	}
+	
+	if ( x >= board_size )
+	{
+		return OUT_OF_BOUNDS ;
+	}
+	
+	if ( y < 0 )
+	{
+		return OUT_OF_BOUNDS ;
+	}
+	
+	if ( y >= board_size )
+	{
+		return OUT_OF_BOUNDS ;
+	}
+	
+	// determine result:
+	
+	char res ;
+	if ( player == 1 )
+	{	
+		res = get_val ( & this -> p2_setup_board , board_size , x , y ) ;
+	}
+	else if ( player == 2 )
+	{	
+		res = get_val ( & this -> p1_setup_board , board_size , x , y ) ;
+	}
+	
+	
+	
+	// return result:
+	
+	if ( res == 'C' || res == 'B' || res == 'R' || res == 'S' || res == 'D' )
+	{
+		return HIT ;
+	}
+	else
+	{
+		return MISS ;
+	}		
 }
 
 
-int Server::process_shot(unsigned int player) {
-   return NO_SHOT_FILE;
+int Server :: process_shot ( unsigned int player )
+{
+	
+	// open json file:
+	
+	string file_in_name ;
+	if ( player == 1 )
+	{		
+		file_in_name = "player_1.shot.json" ;
+	}
+	else if ( player == 2 )
+	{
+		file_in_name = "player_2.shot.json" ;
+	}
+	else
+	{
+		throw ServerException ( "Bad player number!\n" ) ;
+	}	
+
+	// create an input file stream:
+	
+	ifstream file_in ( file_in_name ) ; 								
+	
+	// if file exists:
+	
+	if ( file_in )						
+	{
+		// initialize an archive on the file:
+		
+		cereal :: JSONInputArchive archive_in ( file_in ) ;			
+		
+		// read json file:
+		
+		unsigned int x ;
+		unsigned int y ;
+		
+		// deserialize the array:
+		
+		archive_in ( x , y ) ; 									
+		
+		//close file:
+		
+		file_in . close ( ) ;								
+		
+		// remove shot file:
+		
+		remove ( file_in_name . c_str ( ) ) ;				
+		
+		// pass to evaluate_shot:
+		
+		int res ;
+		if ( player == 1 )
+		{		
+			res = evaluate_shot ( 1 , x , y ) ;
+		}
+		else if ( player == 2 )
+		{
+			res = evaluate_shot ( 2 , x , y ) ;
+		}
+		
+		// write to result file:
+		
+		string file_out_name ;		
+		if ( player == 1 )
+		{		
+			file_out_name = "player_1.result.json" ;
+		}
+		else if ( player == 2 )
+		{
+			file_out_name = "player_2.result.json" ;
+		}
+		
+		// remove any old serialization files:
+		
+		remove ( file_out_name . c_str ( ) ) ;										
+		
+		// create an output file stream:
+		
+		ofstream file_out ( file_out_name ) ; 
+
+		// must include braces so destructor of archive is called before we close the file:
+		{
+			// initialize an archive on the file:
+			
+			cereal :: JSONOutputArchive archive_out ( file_out ) ; 			
+			
+			// serialize the data giving it a name:
+			
+			archive_out ( cereal :: make_nvp ( "result" , res ) ) ; 				
+		}
+		// destructor will have been called
+		
+		// close the file:
+		
+		file_out . close ( ) ; 	
+
+		// return status code:
+		
+		return SHOT_FILE_PROCESSED ;
+	}
+	
+	// brach here if file does not exist:
+	
+	// close file:
+	
+	file_in . close ( ) ;								
+	
+	// remove shot file:
+	
+	remove ( file_in_name . c_str ( ) ) ;				
+		
+	// return error result:
+	
+	return NO_SHOT_FILE ;
 }
